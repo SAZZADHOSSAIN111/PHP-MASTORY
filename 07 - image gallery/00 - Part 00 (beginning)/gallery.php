95d85b821ce8059e8ef3fc60534efaa3713ad567
@SAZZADHOSSAIN111 <mdshazzadhossain@gmail.com>
2026-04-30 19:03:05 +0600

    image gallery

---
+++ 07 - image gallery/00 - Part 00 (beginning)/gallery.php
@@ -0,0 +1,14 @@
+<?php
+include './inc/functions.inc.php';
+include './inc/images.inc.php';
+?>
+<?php include './views/header.php'; ?>
+
+<?php foreach($imageTitles AS $source => $title): ?>
+    <a href="images.php?<?php echo http_build_query(['image' => $source]); ?>" class="gallery-item">
+        <h3><?php echo e($title) ?> </h3>
+        <img src="./images/<?php echo e($source) ?>" alt=" $title">
+    </a>
+<?php endforeach; ?>
+
+<?php include './views/footer.php'; ?>
