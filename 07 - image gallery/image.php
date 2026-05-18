fdcd74f9edb0c8a933e595be4d04a3e3b5a84411
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2026-05-18 22:32:18 +0600

    daily update

--- 07 - image gallery/image.php
+++ 07 - image gallery/image.php
@@ -5,5 +5,6 @@ include './inc/images.inc.php';
 ?>
 <?php include './views/header.php'; ?>
 
+<?php if !empty($_GET[images]) && !empty($imageTitles = [$image])>
 
 <?php include './views/footer.php'; ?>
