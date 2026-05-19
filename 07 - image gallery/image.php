ecb27e7e3fe6116697ce88df00c23053fcee8c65
@SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2026-05-19 12:09:56 +0600

    finish the gallery

--- 07 - image gallery/image.php
+++ 07 - image gallery/image.php
@@ -5,6 +5,19 @@ include './inc/images.inc.php';
 ?>
 <?php include './views/header.php'; ?>
 
-<?php if !empty($_GET[images]) && !empty($imageTitles = [$image])>
+<?php if (!empty($_GET['image']) && !empty($imageTitles[$_GET['image']])): ?>
+    <?php $imasdge= $_GET['image']; ?>
+
+    <h3 class="h3inimage"><?php echo $imageTitles[$_GET['image']]; ?> </h3>
+    
+    <img src="./images/<?php echo rawurldecode($imasdge); ?>" /> 
+    <p class="pinimage"><?php echo str_replace("\n", "<br />", e( $imageDescriptions[$_GET['image']])); ?> </p>
+
+    <?php else: ?>
+        <div class="notiice" >
+            The image is not loading
+        </div>
+
+<?php endif; ?>
 
 <?php include './views/footer.php'; ?>
